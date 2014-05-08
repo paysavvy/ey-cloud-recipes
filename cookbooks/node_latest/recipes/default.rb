96666293121ac7dac8cc2aabb8b1d9f4cfde285d
serj <serj@meskallito.com>
2014-05-08 10:13:15 -0700

    specified user name for execute command

--- cookbooks/node_latest/recipes/default.rb
+++ cookbooks/node_latest/recipes/default.rb
@@ -4,6 +4,7 @@
 #
 if node[:instance_role] == 'app_master'
   execute "installing node version manager" do
+    user node[:users].first[:username]
     command "curl https://raw.githubusercontent.com/creationix/nvm/v0.6.1/install.sh | sh && nvm install 0.10.28 && nvm alias default 0.10.28"
   end
 end
