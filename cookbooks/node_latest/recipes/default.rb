f31f944492f6f24662ade802df38d8f65e1d4699
serj <serj@meskallito.com>
2014-05-08 10:29:14 -0700

    re-init profile after nvm installation

--- cookbooks/node_latest/recipes/default.rb
+++ cookbooks/node_latest/recipes/default.rb
@@ -6,6 +6,6 @@ if node[:instance_role] == 'app_master'
   execute "installing node version manager" do
     user 'deploy'
     environment({ 'HOME' => '/home/deploy' })
-    command "curl https://raw.githubusercontent.com/creationix/nvm/v0.6.1/install.sh | sh && nvm install 0.10.28 && nvm alias default 0.10.28"
+    command "curl https://raw.githubusercontent.com/creationix/nvm/v0.6.1/install.sh | sh && source ~/.bash_profile && nvm install 0.10.28 && nvm alias default 0.10.28"
   end
 end
