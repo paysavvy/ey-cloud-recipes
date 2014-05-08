596ebb125b3fe1b48d8554b3f4cac404e25ff0ff
serj <serj@meskallito.com>
2014-05-08 10:21:51 -0700

    syntax error

--- cookbooks/node_latest/recipes/default.rb
+++ cookbooks/node_latest/recipes/default.rb
@@ -5,7 +5,7 @@
 if node[:instance_role] == 'app_master'
   execute "installing node version manager" do
     user 'deploy'
-    environment { 'HOME' => '/home/deploy' }
+    environment({ 'HOME' => '/home/deploy' })
     command "curl https://raw.githubusercontent.com/creationix/nvm/v0.6.1/install.sh | sh && nvm install 0.10.28 && nvm alias default 0.10.28"
   end
 end
