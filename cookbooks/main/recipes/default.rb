ffab359923967623365179e1e578870ec250c871
serj <serj@meskallito.com>
2014-06-02 14:19:04 -0700

    postgresql extension for kiev

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -113,7 +113,7 @@ require_recipe "node_latest"
 
 
 #enable Extension modules for a given Postgresql database
-# if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
+if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # Extensions that support Postgres >= 9.0
   # postgresql9_autoexplain "dbname"
   # postgresql9_btree_gin "dbname"
@@ -126,7 +126,7 @@ require_recipe "node_latest"
   # postgresql9_dict_xsyn "dbname"
   # postgresql9_earthdistance "dbname"
   # postgresql9_fuzzystrmatch "dbname"
-  # postgresql9_hstore "dbname"
+  postgresql9_hstore "kiev"
   # postgresql9_intarray "dbname"
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
@@ -145,7 +145,7 @@ require_recipe "node_latest"
   # postgresql9_tablefunc "dbname"
   # postgresql9_test_parser "dbname"
   # postgresql9_unaccent "dbname"
-  # postgresql9_uuid_ossp "dbname"
+  postgresql9_uuid_ossp "kiev"
   
   
   # 9.1 and 9.2 Extensions
@@ -158,4 +158,4 @@ require_recipe "node_latest"
   # Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
-# end
+end
